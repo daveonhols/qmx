bb20662c6e138ade324f64414c76b8246640b590
David Smith <daveonhols@github.com>
2022-04-28 21:47:10 +0100

    Make QMX robust on failures, new client test drivers

--- q/worker.q
+++ q/worker.q
@@ -1,7 +1,8 @@
 / "worker" means the processes behind gateway that actually deal with user queries
 / eg rlwrap ~/q/l32/q worker.q -p 8833
 
-.z.ps:.z.pg:{show x; value x};
+.z.ps:.z.pg:{show (-3!.z.p)," :: ",-3!x; value x};
+.z.pc:{show (-3!.z.p)," :: gone away :: ",-3!x};
 tbl:([] a: `float$til 100); / not used now
 tbl2:([] a:(10000000?8 9 10)?\:`char$(`int$"a")+til 26); / make a big table
 
--- src/gw.hpp
+++ src/gw.hpp
@@ -1,9 +1,9 @@
+#include <thread>
+#include <chrono>
+
 namespace qmx {
-
   namespace util {
-
-    namespace kdb {
-      
+    namespace kdb {      
       tcp::socket get_socket(asio::io_context& io, std::string ip, int port) {
 	asio::error_code err;
 	asio::ip::address a_w = asio::ip::make_address(ip);
@@ -50,6 +50,19 @@ namespace qmx {
           b_shift += 8;
 	}
 	return len;
+      }
+
+      std::vector<unsigned char> signal_bytes(std::string signal) {
+	// it's going to be simpler if we don't signal more than 255 chars
+	if(signal.length() > 240) {
+	  signal = signal.substr(0, 240);
+	}
+	char len = 10 + signal.length() & 0xff;
+	std::vector<unsigned char> msg{1, 2, 0, 0, 0, 0, 0, 0, 128};
+	msg[4] = len;
+	std::copy(signal.begin(), signal.end(), std::back_inserter(msg));
+	msg.push_back(0);
+	return msg;
       }      
     }   
   }
@@ -57,6 +70,8 @@ namespace qmx {
   namespace exception {
     class srceof : public std::exception {};
     class dsteof : public std::exception {};
+    class srcbrokenpipe : public std::exception {};
+    class dstbrokenpipe : public std::exception {};    
   }
 
   
@@ -67,8 +82,9 @@ namespace qmx {
 
     public:
       connection(asio::io_context& io, std::string h, int p) : io(io), host(h), port(p) {
-	connect(); 
+	//connect(); 
       }
+
       void connect() {
 	sock = util::kdb::get_socket(io, host, port);
       }
@@ -88,35 +104,44 @@ namespace qmx {
     class owned_connection {
 
     public:
-      owned_connection(connection&& ww, connection_pool& oo) : conn(std::move(ww)), owner(oo), live(true){ }
+      owned_connection(connection&& ww, connection_pool& oo) :failed(false), conn(std::move(ww)), owner(oo){ }
       ~owned_connection();
-      owned_connection(owned_connection&& other) : conn(std::move(other.conn)), owner(other.owner), live(true) {
-	other.live=false; // if moved out of, we are not responsible for putting back worker to owning pool
+      owned_connection(owned_connection&& other) : failed(other.failed), conn(std::move(other.conn)), owner(other.owner)  {
+	other.conn = std::nullopt;
       }
       owned_connection(const owned_connection&) = delete;
       owned_connection& operator=(const owned_connection&) = delete;
       owned_connection& operator=(owned_connection &&other) = delete;
       tcp::socket& socket() {
-	tcp::socket& res = conn.socket();
+	tcp::socket& res = conn->socket();
 	return res;
- 
+      }
+      void fail() {
+        failed = true;
+	conn->socket().close();
       }
     private:
-      bool live;
-      connection conn;
+      bool failed;
+      std::optional<connection> conn{};
       connection_pool& owner;  
     };
 
-    using worker_provider = std::function<qmx::worker::owned_connection ()>; // we will have a lambda matching this signature to get a conn from pool when we need it.
+    // we will have a lambda on a thread / timer that occassionaly tries to bring failed workers back to life.
+    using recover_failed = std::function<void ()>;
+    // we will have a lambda matching this signature to get a conn from pool when we need it.
+    using worker_provider = std::function<qmx::worker::owned_connection ()>; 
 
     class connection_pool {
     private:
       std::deque<qmx::worker::connection> workers{};
+      std::deque<qmx::worker::connection> failed{};
       std::mutex mutex;
       std::condition_variable ready;
     public:
       void add(connection&& w) {
-	workers.push_back(std::move(w));
+	// connections go into failed on start up
+	// means we can have one connect / fail / recover loop on the timer
+	failed.push_back(std::move(w));
       }
 
       connection take() {
@@ -129,15 +154,48 @@ namespace qmx {
 	return res;
       }
 
-      void done(connection w) {
+      recover_failed get_recover_fn() {
+	using namespace std::chrono_literals;
+	return [this]() {
+		 while(true) {		   
+		   std::this_thread::sleep_for(2000ms);
+		   std::unique_lock<std::mutex> guard(mutex);
+		   int to_recover = failed.size();
+		   int recovered = 0;
+		   while(to_recover > 0) {
+		     connection recover = std::move(failed.front());
+		     failed.pop_front();
+     		     to_recover--;
+		     try{
+		       recover.connect();
+		     } catch (std::system_error& err) {
+		       failed.push_back(std::move(recover));
+		       continue;
+		     }
+		     workers.push_back(std::move(recover));
+		     recovered++;
+		   }
+		   if(recovered > 0) {
+		     ready.notify_one();
+		   }
+		 }
+	       };
+      }
+
+      void done(connection c) {
 	std::unique_lock<std::mutex> guard(mutex);
-	workers.push_back(std::move(w));
+	workers.push_back(std::move(c));
 	ready.notify_one();
+      }
+
+      void fail(connection c) {
+	std::unique_lock<std::mutex> guard(mutex);
+	failed.push_back(std::move(c));
       }
 
       worker_provider getter() {
 	return [this]() -> owned_connection {
-		 owned_connection owned(std::move(take()), *this);
+		 owned_connection owned(take(), *this);
 		 return owned;
 	       };
       }  
@@ -161,6 +219,14 @@ namespace qmx {
       return bb;
     }
 
+    void drain_socket(tcp::socket& s, size_t num) {
+      std::vector<char> buff(65536);
+      while(num > 0) {
+        num -= s.read_some(asio::buffer(buff, buff.size()));
+	// do nothing, we are just clearing the socket for this message
+      }
+    }
+
     void transfer_remaining(tcp::socket& from, tcp::socket& to, std::vector<char> start) {
       size_t read = start.size();
       size_t header_required = (read < 8) ? read - 8 : 0;
@@ -171,15 +237,29 @@ namespace qmx {
 	header_required = (read < 8) ? read - 8 : 0;
 	start.insert(start.end(), more.begin(), more.end());
       }
-
       size_t msg_len = qmx::util::kdb::get_msg_length(start);
-      asio::write(to, asio::buffer(start, start.size()));
-      size_t to_write = msg_len - read;
-      while(to_write > 0) {
-	std::vector<char> next_chunk = read_some(from);
-	asio::write(to, asio::buffer(next_chunk, next_chunk.size()));
-	to_write-=next_chunk.size();
-      }      
+      size_t remaining = msg_len - read;
+      try {
+        asio::write(to, asio::buffer(start, start.size()));      
+	while(remaining > 0) {
+	  std::vector<char> next_chunk = read_some(from);
+          remaining-=next_chunk.size();
+	  asio::write(to, asio::buffer(next_chunk, next_chunk.size()));
+	}
+      }catch (std::system_error& err) {      
+	if(0 == std::string(err.what()).compare(std::string("read_some: End of file"))) {  
+	  throw exception::dsteof();
+	}
+	if(0 == std::string(err.what()).compare(std::string("write: Broken pipe"))) {
+	  drain_socket(from, remaining);  // write failed but we need to make sure nothing pending on read side.
+	  throw exception::dstbrokenpipe();
+	}
+	if(0 == std::string(err.what()).compare(std::string("write: Connection reset by peer"))) {
+	  drain_socket(from, remaining);  // write failed but we need to make sure nothing pending on read side.
+	  throw exception::dstbrokenpipe();
+	}
+	throw err;
+      }
     }
   }
 
@@ -193,6 +273,7 @@ namespace qmx {
       }
   
       void exec() {
+        using namespace std::chrono_literals;
 	std::vector<char> buff{};
 	util::kdb::write_capability(client);    
 	std::string user_pass = util::kdb::read_login(client, buff);
@@ -201,16 +282,31 @@ namespace qmx {
 	  std::optional<worker::owned_connection> reply_to;
 	  try {
 	    if(buff.size() > 0) {
-	      std::cout << " **** ** into the long password read code" << std::endl;
 	    }
 	    std::vector<char> chunk = (buff.size() > 0) ? buff : io::read_some(client); // block until client starts
 	    reply_to.emplace(provide()); // take a worker from the pool *after* client query comes in.
 	    io::transfer_remaining(client, (*reply_to).socket(), chunk);
 	  } catch (exception::srceof const& e) {
-	    std::cerr << "client eof ... (went away)" << std::endl;
 	    break;
 	  }
-	  io::transfer_remaining((*reply_to).socket(), client, io::read_some((*reply_to).socket()));
+	  // send response back from worker to client
+	  // src is worker, dst is client 
+	  try {
+	    io::transfer_remaining((*reply_to).socket(), client, io::read_some((*reply_to).socket()));
+	  }
+	  // data coming back from worker to client,
+	  // if the client goes away, data copy won't finish,
+	  // bytes are left hanging on worker outbound socket
+	  // we need to clear them up.
+	  catch (exception::dstbrokenpipe const& e) {
+	    // client went away, we drained the worker already, nothing else to do.
+	  }
+	  catch (exception::srceof const& e) {
+	    // worker went away
+	    (*reply_to).fail();
+	    std::vector<unsigned char> error = util::kdb::signal_bytes("Worker Closed");
+	    asio::write(client, asio::buffer(error, error.size()));
+	  }
 	  buff.clear();
 	}
       }
@@ -227,10 +323,13 @@ namespace qmx {
 
     public:
       server(asio::io_context& io, std::uint16_t port, worker::worker_provider p)
-	: io(io)
+	: prov(p)
+	, io(io)
 	, acceptor(io, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port))
-	, prov(p)
-      {}
+
+      {
+
+      }
 
       void async_accept() {
 	socket.emplace(io);
@@ -250,6 +349,5 @@ namespace qmx {
       std::unordered_map<int, std::thread> threads;
       std::optional<asio::ip::tcp::socket> socket;  
     };
-
   }
 }
